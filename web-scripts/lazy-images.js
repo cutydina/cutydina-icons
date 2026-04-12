d39088349cc2e67f8204896e1e4da945928b30e4
CutyDina <103970174+cutydina@users.noreply.github.com>
2026-04-12 11:25:37 -0600

    Create lazy-images.js

---
+++ web-scripts/lazy-images.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){for(var e=document.querySelectorAll("#page-content-wrapper img"),t=0;t<e.length;t++)t>0&&e[t].setAttribute("loading","lazy")});
